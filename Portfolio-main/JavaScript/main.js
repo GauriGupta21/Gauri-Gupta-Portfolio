8b1c03b9485337a034ec179bd840fa2fcf19008e
Gauri Gupta <117813988+GauriGupta21@users.noreply.github.com>
2024-04-04 13:44:05 +0530

    Uploading files for portfolio

---
+++ Portfolio-main/JavaScript/main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+  strings: ["Full Stack Developer","Event Manager","GDG Campus Ambassdor","CNCF Co-Organizer"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
